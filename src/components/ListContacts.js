be1348ce8743b3165fda7179a4e80e5fcb66086d
Paulo Ziembick <pauloziembick@gmail.com>
2022-12-26 15:19:53 -0300

    search tab

--- src/components/ListContacts.js
+++ src/components/ListContacts.js
@@ -1,29 +1,47 @@
 import PropTypes from "prop-types";
-
+import { useState } from "react";
 
 const ListContacts = ({ contacts, onDeleteContact }) => {
+  const [query, setQuery] = useState("");
+
+  const updateQuery = (query) => {
+    setQuery(query.trim());
+  }
+
   return (
     <div className="list-contacts">
       <div className="list-contacts-top">
-      <input className="search-contacts" type="text" placeholder="Search Contacts"></input>
+        <input
+          className="search-contacts"
+          type="text"
+          placeholder="Search Contacts"
+          value={query}
+          onChange={(event)=> updateQuery(event.target.value) }
+        ></input>
       </div>
       <ol className="contact-list">
-      {contacts.map((contact) => (
-        <li key={contact.id} className="contact-list-item">
-          <div
-            className="contact-avatar"
-            style={{
-              backgroundImage: `url(${contact.avatarURL})`
-            }}
-          ></div>
-          <div className="contact-details">
-            <p>{contact.name}</p>
-            <p>{contact.handle}</p>
-          </div>
-          <button className="contact-remove" onClick={() => onDeleteContact(contact)}>Remove</button>
-        </li>
-      ))}
-    </ol>
+        {console.log(query)}
+        {contacts.map((contact) => (
+          <li key={contact.id} className="contact-list-item">
+            <div
+              className="contact-avatar"
+              style={{
+                backgroundImage: `url(${contact.avatarURL})`,
+              }}
+            ></div>
+            <div className="contact-details">
+              <p>{contact.name}</p>
+              <p>{contact.handle}</p>
+            </div>
+            <button
+              className="contact-remove"
+              onClick={() => onDeleteContact(contact)}
+            >
+              Remove
+            </button>
+          </li>
+        ))}
+      </ol>
     </div>
   );
 };
@@ -31,6 +49,6 @@ const ListContacts = ({ contacts, onDeleteContact }) => {
 ListContacts.propTypes = {
   contacts: PropTypes.array.isRequired,
   onDeleteContact: PropTypes.func.isRequired,
-}
+};
 
 export default ListContacts;
